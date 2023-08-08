8e71a23abb3a482276c3343c80213c9d9559dfeb
Judit Vives <jvivesramon@gmail.com>
2023-08-08 11:52:26 +0200

    Add Home component at AppRouter through lazy loading file

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import pathTo from "./paths/paths";
 import App from "../components/App/App";
+import { Suspense } from "react";
+import { LazyHomePage } from "./lazyPages";
 
 const appRouter = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const appRouter = createBrowserRouter([
       {
         index: true,
         element: <Navigate to={pathTo.homePage} replace />,
+      },
+      {
+        path: pathTo.homePage,
+        element: (
+          <Suspense>
+            <LazyHomePage />
+          </Suspense>
+        ),
       },
     ],
   },
---
+++ src/routers/lazyPages.ts
@@ -0,0 +1,3 @@
+import { lazy } from "react";
+
+export const LazyHomePage = lazy(() => import("../components/Home/Home"));
